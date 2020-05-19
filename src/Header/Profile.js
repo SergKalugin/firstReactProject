c48bff406e84c8cdfb052fc370d7f9dd2eb86a41
Serg Kalugin <kaluginssergio@gmail.com>
2020-05-19 14:18:41 +0300

    Delete package.json

--- src/Header/Profile.js
+++ src/Header/Profile.js
@@ -12,13 +12,13 @@ const Profile = () => {
               <button type="button" class="mad-profile__name dropdown-title" aria-expanded="false">Serg Kalugin</button>
               <div class="mad-profile__dropdown-element" aria-hidden="true">
                 <ul class="mad-profile__list with-icons">
-                  <li><a href="stream.html"><i class="material-icons-outlined">home</i>Stream</a></li>
-                  <li><a href="about.html"><i class="material-icons-outlined">account_circle</i>About</a></li>
-                  <li><a href="blog.html"><i class="material-icons-outlined">library_books</i>Blog</a></li>
-                  <li><a href="classifieds.html"><i class="material-icons-outlined">ballot</i>Classifieds</a></li>
-                  <li><a href="friends.html"><i class="material-icons-outlined">group</i>Friends</a></li>
-                  <li><a href="group.html"><i class="material-icons-outlined">group_work</i>Groups</a></li>
-                  <li><a href="photos.html"><i class="material-icons-outlined">photo_library</i>Photos</a></li>
+                  <li><a href="#"><i class="material-icons-outlined">home</i>Stream</a></li>
+                  <li><a href="#"><i class="material-icons-outlined">account_circle</i>About</a></li>
+                  <li><a href="#"><i class="material-icons-outlined">library_books</i>Blog</a></li>
+                  <li><a href="#"><i class="material-icons-outlined">ballot</i>Classifieds</a></li>
+                  <li><a href="#"><i class="material-icons-outlined">group</i>Friends</a></li>
+                  <li><a href="#"><i class="material-icons-outlined">group_work</i>Groups</a></li>
+                  <li><a href="#"><i class="material-icons-outlined">photo_library</i>Photos</a></li>
                   <li><a href="#"><i class="material-icons-outlined">video_library</i>Audio &amp; Video</a></li>
                   <li><a href="#"><i class="material-icons-outlined">settings</i>Preferences</a></li>
                   <li><a href="#"><i class="material-icons-outlined">meeting_room</i>Log Out</a></li>
